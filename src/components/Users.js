d08ad08a7e43927fe88e98c0d1d7a8781da8d799
Mike Dang <mike.dang@ga.co>
2017-04-25 17:02:21 -0500

    Modified original boiler plate to use create-react-app

---
+++ src/components/Users.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Users extends Component {
+  render() {
+    const users = this.props.users.map((user) => {
+      return (
+        <li
+          key={ user.id }
+          onClick={() => this.props.selectUser(user)}>
+          { user.first } { user.last }
+        </li>
+      );
+    });
+
+    return (
+      <ul>
+        { users }
+      </ul>
+    );
+  }
+}
+
+export default Users;
---
+++ src/reducers/index.js
@@ -0,0 +1,19 @@
+import { combineReducers } from 'redux';
+import UsersReducer from './users_reducer';
+import ActiveUserReducer from './active_user_reducer';
+
+// We combine all reducers into a single object before updated data is dispatched (sent) to store
+//  Your entire applications state (store) is just whatever gets returned from all your reducers
+const rootReducer = combineReducers({
+  users: UsersReducer,
+  activeUser: ActiveUserReducer
+});
+
+/*
+  {
+    'users': [],
+    'activeUser': {}
+  }
+*/
+
+export default rootReducer;
