9f7b2197616d956327a4e2cd6d2aa95d3d535adf
Mike Dang <mike.dang@ga.co>
2017-04-26 10:13:14 -0500

    Refactored file name

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import * as types from '../constants/ActionTypes';
+import * as types from '../constants/action_types';
 
 export const selectUser = (user) => {
   console.log('You clicked on user: ', user);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-import UsersReducer from './users_reducer';
-import ActiveUserReducer from './active_user_reducer';
+import UsersReducer from './users';
+import ActiveUserReducer from './active_user';
 
 // We combine all reducers into a single object before updated data is dispatched (sent) to store
 //  Your entire applications state (store) is just whatever gets returned from all your reducers
